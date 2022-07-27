7ba426da8e2d4b5dee0a59a8e46553efe33336a9
h0gan1ee <lihaojun2003@outlook.com>
2022-07-27 10:31:19 +0800

    feat: cursor

--- client/src/components/BaseButton.vue
+++ client/src/components/BaseButton.vue
@@ -11,7 +11,7 @@ const props = withDefaults(defineProps<Props>(), {
 <template>
   <button
     :disabled="props.disabled"
-    class="py-3 px-6 shadow rounded-xl font-light ease-in-out duration-200 hover:bg-slate-50 focus:bg-slate-50 active:bg-slate-100 disabled:(text-slate-300 bg-slate-50)"
+    class="py-3 px-6 shadow rounded-xl font-light ease-in-out duration-200 hover:bg-slate-50 focus:bg-slate-50 active:bg-slate-100 disabled:(text-slate-300 bg-slate-50 cursor-not-allowed)"
   >
     <slot></slot>
   </button>
