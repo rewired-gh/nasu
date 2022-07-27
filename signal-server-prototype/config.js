822f1ce5d2ee3221884f6850a5ca7d616f868e5f
h0gan1ee <lihaojun2003@outlook.com>
2022-07-27 14:49:08 +0800

    feat: misc

--- signal-server-prototype/config.js
+++ signal-server-prototype/config.js
@@ -2,7 +2,11 @@ const config = {
   maxSessionLength: 128,
   maxStringLength: 0x400,
   portHttp: 9753,
-  allowedOrigins: ["https://nasu.hopp.top", "https://nasu.netlify.app"],
+  allowedOrigins: [
+    "https://nasu.hopp.top",
+    "https://nasu.netlify.app",
+    // "http://localhost:5173",
+  ],
 };
 
 export default config;
